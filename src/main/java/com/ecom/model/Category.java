ce08569afffebd1f40e45c6ab7e406dc0c763f35
Azlan Khatti <azalankhatti786@gmail.com>
2025-10-23 19:46:39 +0500

    Make work project

--- src/main/java/com/ecom/model/Category.java
+++ src/main/java/com/ecom/model/Category.java
@@ -24,4 +24,6 @@ public class Category {
 
     private String name;
     private String imageName;
+
+    private boolean isActive;
 }
--- src/main/java/com/ecom/repository/CategoryRepository.java
+++ src/main/java/com/ecom/repository/CategoryRepository.java
@@ -3,6 +3,10 @@ package com.ecom.repository;
 
 import com.ecom.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
+    public boolean existsByName(String name);
 }
