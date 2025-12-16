9ee7648cf8f1c0bf757a40109159c98c92b5de0a
Azlan Khatti <azalankhatti786@gmail.com>
2025-12-16 12:17:06 +0500

    Spring Security

--- src/main/java/com/ecom/config/SecurityConfig.java
+++ src/main/java/com/ecom/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ecom.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -7,9 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -18,27 +23,31 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsServiceImpl userDetailsService(){
+
         return new UserDetailsServiceImpl();
     }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+      provider.setUserDetailsService(userDetailsService());
+      provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
-        http.csrf(csrf->csrf.disable()).cors(cors->cors.disable())
+        http.csrf(csrf->csrf.disable())
+                .cors(cors->cors.disable())
+                .authenticationProvider(authenticationProvider())
                 .authorizeHttpRequests(req->req.requestMatchers("/user/**").hasRole("USER")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/**").permitAll())
                 .formLogin(form->form.loginPage("/signin")
                         .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/"))
+//                                .defaultSuccessUrl("/")
+                                .successHandler(authenticationSuccessHandler))
                 .logout(logout->logout.permitAll());
 
 
